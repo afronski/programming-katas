6fde6a4a3124fc51e9d07a59fb4332d3aa826a85
Wojciech Gawronski <afronski@gmail.com>
2014-04-13 12:51:24 +0200

    Adding C# and F# starters (powered by MonoDevelop).

---
+++ starters/csharp/ProgrammingKata/Test.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+
+namespace ProgrammingKata
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void TestCase()
+		{
+			Assert.AreEqual (4, 2 + 2);
+		}
+	}
+}
