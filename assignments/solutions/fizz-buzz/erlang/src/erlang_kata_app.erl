3f834bc0c1c9e6b74da8d2f5ebe48b35c953f6db
Wojciech Gawronski <afronski@gmail.com>
2014-04-14 10:38:24 +0200

    Erlang Fizz-Buzz implementation completed.

--- assignments/solutions/fizz-buzz/erlang/src/erlang_kata_app.erl
+++ assignments/solutions/fizz-buzz/erlang/src/erlang_kata_app.erl
@@ -1,5 +1,12 @@
 -module(erlang_kata_app).
--export([ assignment/1 ]).
+-export([ fizzbuzz/1 ]).
 
-assignment(X) ->
-	X * X.
+fizzbuzz(List) ->
+	Replacer =
+		fun
+			(A) when A rem 15 =:= 0 -> "FizzBuzz";
+			(A) when A rem 5 =:= 0 -> "Buzz";
+			(A) when A rem 3 =:= 0 -> "Fizz";
+			(A) -> A
+		end,
+	lists:map(Replacer, List).
--- assignments/solutions/fizz-buzz/erlang/src/erlang_kata_tests.erl
+++ assignments/solutions/fizz-buzz/erlang/src/erlang_kata_tests.erl
@@ -3,8 +3,27 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-passing_test() ->
-	?assertEqual(4, erlang_kata_app:assignment(2)).
+% Helpers.
 
-failing_test() ->
-	?assertEqual(8, erlang_kata_app:assignment(2)).
+listFromTo(From, To) ->
+	lists:seq(From, To).
+
+% Tests.
+
+fizz_for_3_test() ->
+	Result = erlang_kata_app:fizzbuzz(listFromTo(1, 3)),
+	?assertEqual("Fizz", lists:nth(3, Result)).
+
+buzz_for_5_test() ->
+	Result = erlang_kata_app:fizzbuzz(listFromTo(1, 5)),
+	?assertEqual("Buzz", lists:nth(5, Result)).
+
+fizzbuzz_for_15_test() ->
+	Result = erlang_kata_app:fizzbuzz(listFromTo(1, 15)),
+	?assertEqual("FizzBuzz", lists:nth(15, Result)).
+
+fizzbuzz_for_100_test() ->
+	Result = erlang_kata_app:fizzbuzz(listFromTo(1, 100)),
+	?assertEqual(27, length(lists:filter(fun(A) -> A =:= "Fizz" end, Result))),
+	?assertEqual(14, length(lists:filter(fun(A) -> A =:= "Buzz" end, Result))),
+	?assertEqual(6, length(lists:filter(fun(A) -> A =:= "FizzBuzz" end, Result))).
