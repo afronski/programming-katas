665211ff692df88d8449a342c82eceeac9a6a11b
Wojciech Gawronski <afronski@gmail.com>
2014-08-28 19:13:12 +0200

    Java 1.7 starter (build with Gradle Wrapper 2.0).

---
+++ starters/java/src/main/java/programming/kata/Main.java
@@ -0,0 +1,9 @@
+package kata.starter;
+
+public class Main {
+
+	public static void main(String[] arguments) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ starters/java/src/test/java/MainTest.java
@@ -0,0 +1,25 @@
+package kata.starter;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MainTest {
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@After
+	public void tearDown() {
+
+	}
+
+	@Test
+	public void thisTestIsDeliberatelyBroken() {
+		Assert.fail("Fix it!");
+	}
+
+}
